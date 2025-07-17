6c8deb5c88d401d06106f674179727e447a61784
RodrigoBolgheroni <rodrigobolgheroni1@gmail.com>
2025-07-17 18:28:15 -0300

    Commit inicial

---
+++ backend-java-servlet/src/main/java/com/smartdiagnose/servlet/HelloServlet.java
@@ -0,0 +1,14 @@
+package com.smartdiagnose.servlet;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Olá, Servlet funcionando!</h1>");
+    }
+}
